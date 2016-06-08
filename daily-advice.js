3f4913142c339271b8ad64f97e57e0e238d760cd
Jaidev Satish <olimar64@gmail.com>
2016-06-08 09:43:40 -0400

    commit

--- daily-advice.js
+++ daily-advice.js
@@ -24,30 +24,34 @@ if (hours >= 22 && hours < 5){
 	time = 'Hope you are having a good day!';
 }
 
-if (hours >=14 && month >=6 && month <= 9){
-	season = 'It gets hot in the afternoon - don\'t forget to hydrate!';
+if (month >=6 && month <= 9){
 	season2 = 'Soak up that sun!';
-} else if (month >= 6 && month <=9) {
-	season = 'Enjoy your summer, but don\'t forget sunscreen!';
-	season2 = 'Soak up that sun!';
-} else if (hours >= 6 && hours <= 12 && month >= 3 && month <=6) {
-	season = 'What a beautiful spring morning!';
-	season2 = 'Enjoy the warm weather!';
+	if (hours >=14 &&)
+		season = 'It gets hot in the afternoon - don\'t forget to hydrate!';
+	} else {
+		season = 'Enjoy your summer, but don\'t forget sunscreen!';
+	}
 } else if (month >= 3 && month <=6) {
-	season = 'Watch out for that pollen!';
 	season2 = 'Enjoy the warm weather!';
-} else if (hours >= 5 && hours <=8 && month >= 9 && month <=12){
-	season = 'It\'ll be getting dark soon!';
-	season2 = 'Don\'t forget to stay warm!';
+	if (hours >= 6 && hours <= 12 ) {
+		season = 'What a beautiful spring morning!';
+	} else {
+		season = 'Watch out for that pollen!';
+	}
 } else if (month >= 9 && month <=12){
-	season = 'Ah, the colors are a\'changin...';
 	season2 = 'Don\'t forget to stay warm!';
-} else if (hours >= 8 && hours <= 2 && month >= 12 && month <=3){
-	season = 'Don\'t forget to let your taps drip before you sleep!';
+	if (hours >= 5 && hours <=8 ) {
+		season = 'It\'ll be getting dark soon!';
+	} else {
+		season = 'Ah, the colors are a\'changin...';
+	}
+} else if (month >= 12 && month <=3){
 	season2 = 'Stay warm!';
-} else if (month >= 12 && month <=3) {
-	season = 'Bundle up before you head out!';
-	season2 = 'Stay warm!';
+	if (hours >= 8 && hours <= 2 ) {
+		season = 'Don\'t forget to let your taps drip before you sleep!';
+	} else (month >= 12 && month <=3) {
+		season = 'Bundle up before you head out!';
+	}
 }
 
 console.log('Hello! Here is some advice for the day:\n' + dayOfWeek + ' ' + time + '\n' + season + '\n' + season2);
